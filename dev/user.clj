8ce6353c2bd67a0ae8e5d2c9da56b480ef17c55e
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2018-04-21 17:38:17 +1000

    more dev commands in user ns

--- dev/user.clj
+++ dev/user.clj
@@ -3,12 +3,19 @@
     [shadow.cljs.devtools.server :as server]
     [shadow.cljs.devtools.api :as shadow]))
 
-(comment
-
+(defn start-dev!
+  []
   (server/start!)                                           ; starts the shadow repl server
 
-  ; starts the code watcher
+  ; starts the code watchers
   (shadow/watch :devcards)
-  (shadow/watch :app)
+  (shadow/watch :app))
 
+(comment
+  ; change the tag below each time to create a snapshot history
+  (shadow/release-snapshot :app {:tag "0.1"})
+
+  ; run below command to build release artifact
+  (shadow/release :app)
   )
+
