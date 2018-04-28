2f64eda4fa803b42bcc764d3f7cc89268903bbe3
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2018-04-28 15:48:04 +1000

    first release version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stu "0.1.0-SNAPSHOT"
+(defproject stu "0.1.0"
   :description "Visualise builds of Clojurescript artifacts"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
